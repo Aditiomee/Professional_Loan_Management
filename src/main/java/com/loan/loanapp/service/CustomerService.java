0f43d9cd4954eab191ad3b7625601047414c23b5
Shruti Betti <shrutibetti@L-R64WVQC2FJ.local>
2023-08-25 17:54:08 +0530

    Shruti changes

--- src/main/java/com/loan/loanapp/service/CustomerService.java
+++ src/main/java/com/loan/loanapp/service/CustomerService.java
@@ -1,14 +1,17 @@
 package com.loan.loanapp.service;
 
 import java.util.Collection;
+
 import com.loan.loanapp.entity.Customer;
 import com.loan.loanapp.entity.Loan;
 import com.loan.loanapp.exception.CustomerException;
 
 public interface CustomerService {
+	
+	
 	Customer addCustomer(Customer newCustomer)throws CustomerException;
 	
-	Customer deleteCustomerById(Integer id)throws CustomerException;
+	String deleteCustomerById(Integer id)throws CustomerException;
 	
 	Customer updateCustomer(Customer newCustomer)throws CustomerException;
 	
